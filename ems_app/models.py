b17ce6f7bd77a422ec8f1f57b9f036ca83a3ba92
amshukanth <amshukanth@agilecrm.com>
2018-03-21 23:19:12 +0530

    Org and Branches Tables Migration Scripts added

--- ems_app/models.py
+++ ems_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Organization(models.Model):
+	name = models.CharField(max_length=100)
+
+class Org_Branches(models.Model):
+	org = models.ForeignKey(Organization, on_delete=models.CASCADE)
+	branch_name = models.CharField(max_length=100)
