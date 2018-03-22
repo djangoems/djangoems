8282cfee09366fff2740ace3462ebb6f49fa62fb
amshukanth <amshukanth@agilecrm.com>
2018-03-22 23:27:11 +0530

    Org and Branches tables added

--- ems_app/admin.py
+++ ems_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import organization, org_branches
+
+class organizationAdmin(admin.ModelAdmin):
+	class Meta:
+		model = organization
+
+admin.site.register(organization, organizationAdmin)
+
+class org_branchesAdmin(admin.ModelAdmin):
+	class Meta:
+		model = org_branches
+
+admin.site.register(org_branches, org_branchesAdmin)
--- ems_app/models.py
+++ ems_app/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
 # Create your models here.
-class Organization(models.Model):
+class organization(models.Model):
 	name = models.CharField(max_length=100)
+	
+	class Meta:
+		verbose_name = 'Organization'
 
-class Org_Branches(models.Model):
-	org = models.ForeignKey(Organization, on_delete=models.CASCADE)
+	def __str__(self):
+	    return self.name
+
+class org_branches(models.Model):
+	org = models.ForeignKey(organization, on_delete=models.CASCADE)
 	branch_name = models.CharField(max_length=100)
+
+	class Meta:
+		verbose_name = 'Branch'
+		verbose_name_plural = 'Branches'
